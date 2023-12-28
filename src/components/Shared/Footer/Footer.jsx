cc86f17e27420c15bac73bde2f9527907e09662a
QuaziSamiha <1703155@student.ruet.ac.bd>
2023-12-28 10:51:00 -0800

    footer done

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,44 +1,52 @@
+import logo from "../../../assets/logo.svg";
+import { FaGoogle, FaLinkedin } from "react-icons/fa";
+import { IoLogoTwitter } from "react-icons/io";
+import { IoLogoInstagram } from "react-icons/io5";
+
 const Footer = () => {
   return (
     <>
-      <footer className="footer p-10 bg-base-200 text-base-content">
+      <footer className="footer p-10 bg-[#151515] text-white">
         <aside>
-          <svg
-            width="50"
-            height="50"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            className="fill-current"
-          >
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
-          <p>
-            ACME Industries Ltd.
-            <br />
-            Providing reliable tech since 1992
+          <img src={logo} className="w-[79px] h-[64.09px]" alt="" />
+          <p className="w-[283px] text-base leading-[26px] font-normal">
+            Edwin Diaz is a software and web technologies engineer, a life coach
+            trainer who is also a serial .
           </p>
+          <div className="flex">
+            <div className="bg-[#2c2c2c] mr-2 w-9 h-9 rounded-full flex justify-center items-center">
+              <FaGoogle className="text-[13.85px]" />
+            </div>
+            <div className="bg-[#2c2c2c] mr-2 w-9 h-9 rounded-full flex justify-center items-center">
+              <IoLogoTwitter className="text-[13.85px]" />
+            </div>
+            <div className="bg-[#2c2c2c] mr-2 w-9 h-9 rounded-full flex justify-center items-center">
+              <IoLogoInstagram className="text-[13.85px]" />
+            </div>
+            <div className="bg-[#2c2c2c] mr-2 w-9 h-9 rounded-full flex justify-center items-center">
+              <FaLinkedin className="text-[13.85px]" />
+            </div>
+          </div>
         </aside>
         <nav>
-          <header className="footer-title">Services</header>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <header className="mb-3 font-semibold text-xl">Services</header>
+          <a className="link link-hover text-base">Branding</a>
+          <a className="link link-hover text-base">Design</a>
+          <a className="link link-hover text-base">Marketing</a>
+          <a className="link link-hover text-base">Advertisement</a>
         </nav>
         <nav>
-          <header className="footer-title">Company</header>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <header className="mb-3 font-semibold text-xl">Company</header>
+          <a className="link link-hover text-base">About us</a>
+          <a className="link link-hover text-base">Contact</a>
+          <a className="link link-hover text-base">Jobs</a>
+          <a className="link link-hover text-base">Press kit</a>
         </nav>
         <nav>
-          <header className="footer-title">Legal</header>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <header className="mb-3 font-semibold text-xl">Legal</header>
+          <a className="link link-hover text-base">Terms of use</a>
+          <a className="link link-hover text-base">Privacy policy</a>
+          <a className="link link-hover text-base">Cookie policy</a>
         </nav>
       </footer>
     </>
