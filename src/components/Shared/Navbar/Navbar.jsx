b63c9da23ed2c45a600258d70018389f5e44040e
QuaziSamiha <1703155@student.ruet.ac.bd>
2023-12-28 10:59:19 -0800

    navbar modified

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -8,33 +8,42 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const navItems = (
+    <>
+      <li>
+        <Link>Home</Link>
+      </li>
+      <li>
+        <Link>About</Link>
+      </li>
+      <li>
+        <Link>Services</Link>
+      </li>
+      <li>
+        <Link>Blog</Link>
+      </li>
+      <li>
+        <Link>contact</Link>
+      </li>
+    </>
+  );
   return (
     <>
       <div className="navbar bg-base-100 my-8">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} className="btn lg:hidden">
-              {isOpen ? <RxCross2 onClick={() => setIsOpen(false)} /> : <FaBars onClick={() => setIsOpen(true)}/>}
+              {isOpen ? (
+                <RxCross2 onClick={() => setIsOpen(false)} />
+              ) : (
+                <FaBars onClick={() => setIsOpen(true)} />
+              )}
             </div>
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-[#444444] font-semibold text-lg"
             >
-              <li>
-                <Link>Home</Link>
-              </li>
-              <li>
-                <Link>About</Link>
-              </li>
-              <li>
-                <Link>Services</Link>
-              </li>
-              <li>
-                <Link>Blog</Link>
-              </li>
-              <li>
-                <Link>contact</Link>
-              </li>
+              {navItems}
             </ul>
           </div>
           <div>
@@ -42,23 +51,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="navbar-center hidden lg:flex text-[#444444] font-semibold text-lg">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <Link>Home</Link>
-            </li>
-            <li>
-              <Link>About</Link>
-            </li>
-            <li>
-              <Link>Services</Link>
-            </li>
-            <li>
-              <Link>Blog</Link>
-            </li>
-            <li>
-              <Link>contact</Link>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{navItems}</ul>
         </div>
         <div className="navbar-end">
           <div className="w-[68px] flex text-[#444444]">
