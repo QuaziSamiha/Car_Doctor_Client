49a64c2c6787aef185f4c5be66137046e19d908a
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-01-02 09:45:44 -0800

    navbar added to error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,21 +1,25 @@
 import { useRouteError } from "react-router-dom";
 import image from "../../assets/images/ErrorPage/Frame.png";
+import Navbar from "../../components/Shared/Navbar/Navbar";
 const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page" className="flex justify-center items-center my-28">
-      <div>
-        <img src={image} alt="" className="h-[280px]" />
-        <h1 className="text-center text-[#ff3811] my-4 text-lg font-bold">
-          Oops!
-        </h1>
-        <p className="text-center text-[#99220a] text-2xl font-bold">
-          <i>{error.statusText || error.message}</i>
-        </p>
+    <>
+      <Navbar />
+      <div id="error-page" className="flex justify-center items-center my-16">
+        <div>
+          <img src={image} alt="" className="h-[280px]" />
+          <h1 className="text-center text-[#ff3811] my-4 text-lg font-bold">
+            Oops!
+          </h1>
+          <p className="text-center text-[#99220a] text-2xl font-bold">
+            <i>{error.statusText || error.message}</i>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
